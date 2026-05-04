7f0b2216fa06034558634b502f72d37c076ff698
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer2809fl@users.noreply.github.com>
2026-05-03 23:50:03 -0600

    Crear readmore.js

---
+++ src/commands/tools/readmore.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, args }) => {
+    const text = args.join(' ') || 'Hola mundo'
+    const rm = String.fromCharCode(8206).repeat(4001)
+    conn.reply(m.chat, `${text}${rm}${text}`, m)
+}
+
+handler.tags = ['tools']
+handler.help = ['readmore']
+handler.command = ['readmore', 'rm', 'leermas']
+handler.register = true
+
+export default handler
